954c53f76c3f270d4e26142a757f62cebcb5ad38
whitney-higgins <whitneyxhiggins@gmail.com>
2023-03-15 14:01:35 -0500

    task 1 completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fifaData } = require('./fifa.js')
+const { fifaData } = require("./fifa.js");
 
 // ⚽️ M  V P ⚽️ //
 
@@ -6,17 +6,30 @@ const { fifaData } = require('./fifa.js')
 Practice accessing data by console.log-ing the following pieces of data note. 
 
 💡 HINT: You may want to filter the data first 😉*/
+const filterFinalHomeTeam = fifaData.filter((team) => {
+  
+  return team.Stage === 'Final' && team.Year === 2014
+  
+ })
+ //console.log(filterFinalHomeTeam)
 
 //(a) Home Team name for 2014 world cup final
 
+console.log(filterFinalHomeTeam[0]["Home Team Name"])
+
 //(b) Away Team name for 2014 world cup final
+
+console.log(filterFinalHomeTeam[0]["Away Team Name"])
 
 //(c) Home Team goals for 2014 world cup final
 
+console.log(filterFinalHomeTeam[0]["Home Team Goals"])
+
 //(d) Away Team goals for 2014 world cup final
 
-//(e) Winner of 2014 world cup final */
+console.log(filterFinalHomeTeam[0]["Away Team Goals"])
 
+//(e) Winner of 2014 world cup final */
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -27,10 +40,8 @@ Use getFinals to do the following:
 */
 
 function getFinals(/* code here */) {
-    /* code here */
- }
-
-
+  /* code here */
+}
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -39,10 +50,8 @@ Use the higher-order function called getYears to do the following:
 3. Return an array called years containing all of the years in the getFinals data set*/
 
 function getYears(/* code here */) {
-    /* code here */
+  /* code here */
 }
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
@@ -50,13 +59,11 @@ Use the higher-order function getWinners to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Determines the winner (home or away) of each `finals` game. 
 💡 HINT: Don't worry about ties for now (Please see the README file for info on ties for a stretch goal.)
-4. Returns the names of all winning countries in an array called `winners` */ 
+4. Returns the names of all winning countries in an array called `winners` */
 
 function getWinners(/* code here */) {
-    /* code here */
+  /* code here */
 }
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use the higher-order function getWinnersByYear to do the following:
@@ -70,10 +77,8 @@ Use the higher-order function getWinnersByYear to do the following:
  */
 
 function getWinnersByYear(/* code here */) {
-    /* code here */
+  /* code here */
 }
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher order function `getAverageGoals` to do the following: 
@@ -90,11 +95,8 @@ Use the higher order function `getAverageGoals` to do the following:
 */
 
 function getAverageGoals(/* code here */) {
-    /* code here */
- }
-
-
-
+  /* code here */
+}
 
 /// 🥅 STRETCH 🥅 ///
 
@@ -105,47 +107,36 @@ Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
 function getCountryWins(/* code here */) {
-
-    /* code here */
-
+  /* code here */
 }
-
-
 
 /* 💪💪💪💪💪 Stretch 2: 💪💪💪💪💪 
 Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
 function getGoals(/* code here */) {
-
-    /* code here */
-
+  /* code here */
 }
-
 
 /* 💪💪💪💪💪 Stretch 3: 💪💪💪💪💪
 Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
-
-    /* code here */
-
+  /* code here */
 }
-
 
 /* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
 
-
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-    console.log('its working');
-    return 'bar';
+function foo() {
+  console.log("its working");
+  return "bar";
 }
 foo();
 module.exports = {
-    foo,
-    getFinals,
-    getYears,
-    getWinners,
-    getWinnersByYear,
-    getAverageGoals
-}
+  foo,
+  getFinals,
+  getYears,
+  getWinners,
+  getWinnersByYear,
+  getAverageGoals,
+};
